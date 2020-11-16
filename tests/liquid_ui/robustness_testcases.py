5d6296d9272dc19ef24c1fb668a5687f85c72cf2
monster.zhou <monster.zhou@ekt-digital.com>
2020-11-16 15:25:16 +0800

    add test_100_times_channel_switch

--- tests/liquid_ui/robustness_testcases.py
+++ tests/liquid_ui/robustness_testcases.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
     DSN5414a General loader test
     Author: ivan.zhao@ekt-digital.com
@@ -8,7 +10,7 @@ import sys
 import time
 import stbt
 import utils
-from dsn5414a import v5utils
+from liquid_ui import v5utils
 
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, parentdir)
@@ -17,7 +19,12 @@ from ektlib import ekt_dta, ekt_mod, ekt_diseqc, ekt_status, ekt_button, ekt_rds
 
 def test_100_times_channel_switch():
     """
+    1、播放码流(测试开始前先执行smoke test的case，进行锁台操作)
+    2、连续切台100次
+    3、检测测试是否成功
     100 times of channel switch with the fastest speed
     :return: None
     """
-    pass
+    for _ in range(100):
+        stbt.press("KEY_DOWN")
+    stbt.wait_for_motion(timeout_secs=3)
